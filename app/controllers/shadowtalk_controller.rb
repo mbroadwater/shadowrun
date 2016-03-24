ef5230867e6ff1da5c852e23c1474ac502dcaecc
Michael Broadwater <mike.broadwater@gmail.com>
2016-03-24 16:12:56 -0400

    Param viewing

--- app/controllers/shadowtalk_controller.rb
+++ app/controllers/shadowtalk_controller.rb
@@ -1,5 +1,12 @@
 class ShadowtalkController < ApplicationController
   def show
-   render :json => {"response_type": "in_channel", "text": "Progress!"}
+   # render :json => {"response_type": "in_channel", "text": "Progress!"}
+   logger.debug (@params)
+
+   @reply ={
+       "response_type": "in_channel",
+       "text" => "Progress!",
+     }.to_json
+
   end
 end
