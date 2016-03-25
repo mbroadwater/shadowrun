c9ef94263c853340f62e65f0fb843734e3875503
Michael Broadwater <mike.broadwater@gmail.com>
2016-03-24 21:16:01 -0400

    Formatting
    again

--- app/controllers/shadowtalk_controller.rb
+++ app/controllers/shadowtalk_controller.rb
@@ -44,7 +44,7 @@ class ShadowtalkController < ApplicationController
       render :json =>
       {
         "response_type": "in_channel",
-        "text": "&lt; &lt; &lt; &lt; &lt; [#{params['text']}]&gt; &gt; &gt; &gt; &gt; - #{toon_name} <#{post_time.strftime("%H:%M:%S / %m-%d-%Y")}>"
+        "text": "&gt;&gt;&gt;&gt;&gt;[#{params['text']}]&lt;&lt;&lt;&lt;&lt; - #{toon_name} <#{post_time.strftime("%H:%M:%S / %m-%d-%Y")}>"
       }
       # logger.debug reply_text
     end
