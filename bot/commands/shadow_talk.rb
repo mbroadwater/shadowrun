57d808811368b18e07fe70adce753ff1610cc50e
Michael Broadwater <mike.broadwater@gmail.com>
2016-04-08 14:36:56 -0400

    debugging

--- bot/commands/shadow_talk.rb
+++ bot/commands/shadow_talk.rb
@@ -11,14 +11,13 @@ module SlackGamebot
 
       def self.call(client, data, match)
         if data.channel == CHANNEL_ID_PRIVATE or data.channel == CHANNEL_ID_TEST
-          reponse = HTTParty.post("https://slack.com/api/chat.delete",
+          logger.debug(HTTParty.post("https://slack.com/api/chat.delete",
             :query => {
               :token => Rails.application.secrets.slack_secret_key,
               :ts => data.ts,
               :channel => data.channel
             },
-            headers: {'Content-Type' => 'application/json'})
-          logger.debug(response.inspect)
+            headers: {'Content-Type' => 'application/json'}))
 
           client.say(channel: data.channel, text: create_comment_text(data.user, data.text))
         end
