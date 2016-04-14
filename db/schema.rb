a1c53840fd233d0b0a9af2eba206c8ad2010bc9a
Michael Broadwater <mike.broadwater@gmail.com>
2016-04-13 20:51:39 -0400

    user signup

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,6 @@ ActiveRecord::Schema.define(version: 20160413215947) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "static_pages", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
