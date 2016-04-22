46a95e02e9511d0d0a88e682024f07cd0c01fb50
Michael Broadwater <mike.broadwater@gmail.com>
2016-04-22 16:07:09 -0400

    already seeded admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 # Create the admin user
 
-User.create!(
-  name:  "Michael Broadwater",
-  email: "mike.broadwater@gmail.com",
-  password: "test1234",
-  password_confirmation: "test1234",
-  admin: true
-)
+# User.create!(
+#   name:  "Michael Broadwater",
+#   email: "mike.broadwater@gmail.com",
+#   password: "test1234",
+#   password_confirmation: "test1234",
+#   admin: true
+# )
 
 # Seed the base attribute table
 BaseAttribute.create!(
