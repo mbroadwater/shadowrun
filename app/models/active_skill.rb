23642330d4214aa50ea87eb7c3c179deebf84e81
Michael Broadwater <mike.broadwater@gmail.com>
2016-04-28 09:31:40 -0400

    Remove uniqueness for skills

--- app/models/active_skill.rb
+++ app/models/active_skill.rb
@@ -3,5 +3,4 @@ class ActiveSkill < ActiveRecord::Base
   belongs_to :base_skill
   has_many :skill_specialties
   default_scope -> { order(base_skill_id: :asc) }
-  validates_uniqueness_of :base_skill_id
 end
