677c6f0d19f5d0f0c7f45b91f50d72ffd827cb66
Michael Broadwater <mike.broadwater@gmail.com>
2016-04-28 09:30:08 -0400

    Remove uniqueness from attributes and skills

--- app/models/char_attribute.rb
+++ app/models/char_attribute.rb
@@ -2,5 +2,4 @@ class CharAttribute < ActiveRecord::Base
   belongs_to :character
   belongs_to :base_attribute
   default_scope -> { order(base_attribute_id: :asc) }
-  validates_uniqueness_of :base_attribute_id
 end
